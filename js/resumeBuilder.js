3d98cd0b2abc71807f57760846178e365e90ae6b
Uma <mahimavai2000@gmail.com>
2017-06-02 22:44:32 -0400

    Corrected errors and Updated

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -71,38 +71,34 @@ bio.display();
 
 var education = {
     "schools": [{
-            "name": "Holy Angels School",
-            "location": "Salem, Tamilnadu, India",
-            "degree": "Secondary School Education",
-            "major": "Computer Science",
-            "dates": "1998",
-            "url": "http://example.com"
-        },
-        {
-            "name": "Golden Gates School",
-            "location": "Chennai, Tamilnadu, India",
-            "degree": "Higher Secondary Education",
-            "major": "Computer Science",
-            "dates": "1998-2000",
-            "url": "http://example.com"
-        },
-        {
-            "name": "Sarada College",
-            "location": "Salem, Tamilnadu, India",
-            "degree": "Bachelor of Science",
-            "major": "Computer Science",
-            "dates": "2000-2003",
-            "url": "http://example.com"
-        },
-        {
-            "name": "Vysya College",
-            "location": "Chennai, Tamilnadu, India",
-            "degree": "Masters of Science",
-            "major": "Computer Science",
-            "dates": "2003-2005",
-            "url": "http://example.com"
-        }
-    ],
+        "name": "Holy Angels School",
+        "location": "Salem, Tamilnadu, India",
+        "degree": "Secondary School Education",
+        "major": "Computer Science",
+        "dates": "1998",
+        "url": "http://example.com"
+    }, {
+        "name": "Golden Gates School",
+        "location": "Chennai, Tamilnadu, India",
+        "degree": "Higher Secondary Education",
+        "major": "Computer Science",
+        "dates": "1998-2000",
+        "url": "http://example.com"
+    }, {
+        "name": "Sarada College",
+        "location": "Salem, Tamilnadu, India",
+        "degree": "Bachelor of Science",
+        "major": "Computer Science",
+        "dates": "2000-2003",
+        "url": "http://example.com"
+    }, {
+        "name": "Vysya College",
+        "location": "Chennai, Tamilnadu, India",
+        "degree": "Masters of Science",
+        "major": "Computer Science",
+        "dates": "2003-2005",
+        "url": "http://example.com"
+    }],
 
     "onlineCourses": {
         "title": "Front End Development",
@@ -144,20 +140,18 @@ education.display();
 
 var work = {
     "jobs": [{
-            "employer": "Solverminds Solutions & Technologies Pvt Ltd",
-            "title": "Software Test Engineer",
-            "location": "Chennai, Tamilnadu, India",
-            "dates": "From May 08’ to Sep 11’",
-            "description": "Solverminds is a software and analytics solution company specializing in providing enterprise application and analytical solutions for maritime transport. Since its inception in September 2003, the company has been in the forefront of delivering innovative solutions to its customers globally. The various accolades the company received over the past years are testimony of its achievements."
-        },
-        {
-            "employer": "Oracle India Pvt Ltd",
-            "title": "Senior QA Consultant",
-            "location": "Hyderabad, Andhra Pradesh, India",
-            "dates": "From Sep 12’ to Sep 15’",
-            "description": "Through our acquisition activities, Oracle seeks to strengthen its product offerings, accelerate innovation, meet customer demand more rapidly, and expand partner opportunities. An integral part of Oracle's Mergers and Acquisitions philosophy is our consistent commitment to customer service and product support while achieving our financial return objectives and creating value for our shareholders."
-        }
-    ]
+        "employer": "Solverminds Solutions & Technologies Pvt Ltd",
+        "title": "Software Test Engineer",
+        "location": "Chennai, Tamilnadu, India",
+        "dates": "From May 08’ to Sep 11’",
+        "description": "Solverminds is a software and analytics solution company specializing in providing enterprise application and analytical solutions for maritime transport. Since its inception in September 2003, the company has been in the forefront of delivering innovative solutions to its customers globally. The various accolades the company received over the past years are testimony of its achievements."
+    }, {
+        "employer": "Oracle India Pvt Ltd",
+        "title": "Senior QA Consultant",
+        "location": "Hyderabad, Andhra Pradesh, India",
+        "dates": "From Sep 12’ to Sep 15’",
+        "description": "Through our acquisition activities, Oracle seeks to strengthen its product offerings, accelerate innovation, meet customer demand more rapidly, and expand partner opportunities. An integral part of Oracle's Mergers and Acquisitions philosophy is our consistent commitment to customer service and product support while achieving our financial return objectives and creating value for our shareholders."
+    }]
 };
 work.display = function() {
     $("#workExperience").append(HTMLworkStart);
@@ -179,34 +173,30 @@ work.display();
 
 var projects = {
     "projects": [{
-            "title": "TRUST",
-            "dates": "May’08 – July 10’",
-            "description": "1st Phase – Vessel scheduling, 2nd Phase – Pricing and Contribution, 3rd Phase – Export, 4th Phase – Import, 5th Phase – Cost, 6th Phase – Equipment management systems More than 3000 users worldwide use this Application. By using the HTTP protocol, the application is accessible worldwide.",
-            "location": "Solverminds Solutions & Technologies Pvt Ltd, Chennai",
-            "images": ["images/shipping.jpg", "images/shipping1.jpg"]
-        },
-        {
-            "title": "TRUST E-Commerce",
-            "dates": "Aug’10 – Sep 11’",
-            "description": "The purpose of the project is to build a competitive e-commerce platform to provide online shipping functions to customers, utilizing the global TRUST system that is in the process of being implemented. TRUST will be the single source where all customer master data will exist. The focus on ecommerce will be to allow the customer to make a booking, send in his shipping instruction, approve the bill of lading and view his invoices and statements. E-commerce will also provide online shipment progress facilities. The system will cater to the needs of the Import cycle. It will assist in the preparation of all the import documentation, invoices and the receipt of payments.",
-            "location": "Solverminds Solutions & Technologies Pvt Ltd, Chennai",
-            "images": ["images/ecom.jpg", "images/ecom1.jpg"]
-        },
-        {
-            "title": "InForm",
-            "dates": "Sep’12 – Sep 14’",
-            "description": "The project involves Clinical Trial development/customization activities in InForm™ base products such as InForm Architect, Oracle 9i, and InForm Database.Develops and Markets data collection and management solutions that enables bio-Pharmaceutical companies, medical device companies, CROs and AROs to integrate and automate the management of their entire clinical development process. EDC (Electronic Data Capture) called Inform delivers the comprehensive clinical trial solutions needed to track product efficacy and safety-from study initiation through FDA submission. The InForm product offers a complete set of features for streamlining data entry and Lab loading, discrepancy management and resolution, patient and site reporting, data exporting, coding and system administration. InForm is a roles-based clinical database Management system that allows users access to the areas of functionality they are permitted to use.",
-            "location": "Oracle India Pvt Ltd, Hyderabad ",
-            "images": ["images/health.jpg", "images/health1.jpg"]
-        },
-        {
-            "title": "InForm-Integration",
-            "dates": "Oct’14 – Sep 15’",
-            "description": "The project involves Clinical Trial development/customization activities in InForm™ base products such as InForm Architect, Oracle 9i, and InForm Database.Develops and Markets data collection and management solutions that enables bio-Pharmaceutical companies, medical device companies, CROs and AROs to integrate and automate the management of their entire clinical development process. EDC (Electronic Data Capture) called Inform delivers the comprehensive clinical trial solutions needed to track product efficacy and safety-from study initiation through FDA submission. The InForm product offers a complete set of features for streamlining data entry and Lab loading, discrepancy management and resolution, patient and site reporting, data exporting, coding and system administration. InForm is a roles-based clinical database Management system that allows users access to the areas of functionality they are permitted to use.",
-            "location": "Oracle India Pvt Ltd, Hyderabad ",
-            "images": ["images/health3.jpg", "images/health4.jpg"]
-        }
-    ]
+        "title": "TRUST",
+        "dates": "May’08 – July 10’",
+        "description": "1st Phase – Vessel scheduling, 2nd Phase – Pricing and Contribution, 3rd Phase – Export, 4th Phase – Import, 5th Phase – Cost, 6th Phase – Equipment management systems More than 3000 users worldwide use this Application. By using the HTTP protocol, the application is accessible worldwide.",
+        "location": "Solverminds Solutions & Technologies Pvt Ltd, Chennai",
+        "images": ["images/shipping.jpg", "images/shipping1.jpg"]
+    }, {
+        "title": "TRUST E-Commerce",
+        "dates": "Aug’10 – Sep 11’",
+        "description": "The purpose of the project is to build a competitive e-commerce platform to provide online shipping functions to customers, utilizing the global TRUST system that is in the process of being implemented. TRUST will be the single source where all customer master data will exist. The focus on ecommerce will be to allow the customer to make a booking, send in his shipping instruction, approve the bill of lading and view his invoices and statements. E-commerce will also provide online shipment progress facilities. The system will cater to the needs of the Import cycle. It will assist in the preparation of all the import documentation, invoices and the receipt of payments.",
+        "location": "Solverminds Solutions & Technologies Pvt Ltd, Chennai",
+        "images": ["images/ecom.jpg", "images/ecom1.jpg"]
+    }, {
+        "title": "InForm",
+        "dates": "Sep’12 – Sep 14’",
+        "description": "The project involves Clinical Trial development/customization activities in InForm™ base products such as InForm Architect, Oracle 9i, and InForm Database.Develops and Markets data collection and management solutions that enables bio-Pharmaceutical companies, medical device companies, CROs and AROs to integrate and automate the management of their entire clinical development process. EDC (Electronic Data Capture) called Inform delivers the comprehensive clinical trial solutions needed to track product efficacy and safety-from study initiation through FDA submission. The InForm product offers a complete set of features for streamlining data entry and Lab loading, discrepancy management and resolution, patient and site reporting, data exporting, coding and system administration. InForm is a roles-based clinical database Management system that allows users access to the areas of functionality they are permitted to use.",
+        "location": "Oracle India Pvt Ltd, Hyderabad ",
+        "images": ["images/health.jpg", "images/health1.jpg"]
+    }, {
+        "title": "InForm-Integration",
+        "dates": "Oct’14 – Sep 15’",
+        "description": "The project involves Clinical Trial development/customization activities in InForm™ base products such as InForm Architect, Oracle 9i, and InForm Database.Develops and Markets data collection and management solutions that enables bio-Pharmaceutical companies, medical device companies, CROs and AROs to integrate and automate the management of their entire clinical development process. EDC (Electronic Data Capture) called Inform delivers the comprehensive clinical trial solutions needed to track product efficacy and safety-from study initiation through FDA submission. The InForm product offers a complete set of features for streamlining data entry and Lab loading, discrepancy management and resolution, patient and site reporting, data exporting, coding and system administration. InForm is a roles-based clinical database Management system that allows users access to the areas of functionality they are permitted to use.",
+        "location": "Oracle India Pvt Ltd, Hyderabad ",
+        "images": ["images/health3.jpg", "images/health4.jpg"]
+    }]
 };
 projects.display = function() {
     $("#projects").append(HTMLprojectStart);
